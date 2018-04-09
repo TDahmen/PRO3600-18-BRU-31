32dd11279ac94db5baa02b87fcc135287d34a42a
Thomas Dahmen <thomas.dahmen@telecom-sudparis.eu>
2018-04-09 21:28:04 +0200

    Data management bis

--- Interface Graphique/dataRecover.py
+++ Interface Graphique/dataRecover.py
@@ -5,7 +5,9 @@ with open("convertedGames.txt") as f:
     content = f.read().splitlines()
     listeJeux = [list(x[1]) for x in itertools.groupby(content, lambda x: x=='NOUVEAU JEU') if not x[0]]
     listeJeuxBis = []
+    k = 0
     for jeu in listeJeux:
+        k = k+1
         l = []
         for coup in jeu:
             u = coup.split(' ')
@@ -15,8 +17,9 @@ with open("convertedGames.txt") as f:
             plateauBis = []
             for value in plateau:
                 if not('NOUVEAU' in value) and not('COUP' in value):
-                    plateauBis.append(value)
+                    plateauBis.append(float(value))
             lBis.append(plateauBis)
         listeJeuxBis.append(lBis)
     # print(listeJeuxBis, file=open("recoveredData.txt", "a"))
     print(listeJeuxBis)
+    print(k)
